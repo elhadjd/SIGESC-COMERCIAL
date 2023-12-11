b265a23753e11cc3ce0d19af33669f8dc8c3797d
Wilson Manuel <gordolinosamukai2001@gmail.com>
2023-12-11 00:06:09 +0000

    ordenate migrate

--- database/migrations/2023_03_11_193708_create_produtos_table.php
+++ database/migrations/2023_03_11_193708_create_produtos_table.php
@@ -13,26 +13,26 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->string('nome')->nullable()->default(NULL);
+            $table->string('nome')->nullable();
             $table->unsignedBigInteger('company_id');
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('CASCADE');
             $table->string('image')->default('produto-sem-imagem.png');
-            $table->string('codego')->nullable()->default(NULL);
+            $table->string('codego')->nullable();
             $table->integer('category_product_id')->nullable();
-            $table->integer('product_type_id')->nullable()->default(NULL);
+            $table->integer('product_type_id')->nullable();
             $table->unsignedBigInteger('sub_category_id');
             $table->foreign('sub_category_id')->references('id')->on('sub_categories');
-            $table->string('fabricante')->nullable()->default(NULL);
+            $table->string('fabricante')->nullable();
             $table->float('preçocust')->default(0);
-            $table->string('imposto')->nullable()->default(NULL);
+            $table->string('imposto')->nullable();
             $table->float('preçovenda')->default(0);
             $table->float('preco_medio')->default(0);
             $table->integer('qtd')->nullable()->default(0);
             $table->float('total_cust')->default(0);
-            $table->enum('inventory',['true','false'])->default('false');
-            $table->boolean('shop_online')->default('false');
-            $table->timestamp('datafab')->nullable()->default(NULL);
-            $table->timestamp('dataexp')->nullable()->default(NULL);
+            $table->boolean('inventory')->default(false);
+            $table->boolean('shop_online')->default(false);
+            $table->timestamp('datafab')->nullable();
+            $table->timestamp('dataexp')->nullable();
             $table->enum('estado',['active','inactive'])->default('active');
             $table->timestamps();
         });
